c772e9f39d167b8528533abc06601cd0a1ee65ed
Rodrigo Ruiz Baca <rodrigo.ruiz.baca@gmail.com>
2018-10-15 12:53:25 -0500

    write into dynamodb

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM alpine:3.7
 WORKDIR /usr/local/src/
 RUN apk add nodejs && apk add git
 RUN git clone https://github.com/rodrigoruizbaca/fargate-nodejs-test.git
-CMD node fargate-nodejs-test/index.js
+WORKDIR /usr/local/src/fargate-nodejs-test
+RUN npm install
+CMD node index.js
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+var AWS = require("aws-sdk");
+AWS.config.update({
+    region: "us-west-2"
+});
+
+const uuid4 = require("uuid").v4;
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+var created_at = new Date().toISOString();
+var params = {
+    TableName : "taskTable",
+    Item: {
+        taskId: uuid4(),
+        created_at            
+    }
+};
+docClient.put(params, function (err, data) {
+  if (err) {
+      throw err;
+  } else {
+      
+  }
+});
+   
